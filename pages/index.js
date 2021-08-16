017accd67595c9cb4c5204311bcb45e14e446ae0
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-08-15 22:31:28 -0300

    Change the file name when select a file

--- pages/index.js
+++ pages/index.js
@@ -27,15 +27,25 @@ export default function Home() {
                 <div className='control'>
                   <div className="file is-large is-link has-name is-boxed">
                     <label className="file-label">
-                      <input className="file-input" type="file" name="resume" />
+                      <input className="file-input" type="file" name="resume" onChange={(event) => {
+                        const fileName = document.querySelector('.file-name');
+
+                        if (event.target.files.length > 0) {
+                          fileName.textContent = event.target.files[0].name;
+                        } else {
+                          fileName.textContent = 'No file selected'
+                        }
+                      }} />
                       <span className="file-cta">
                         <span className="file-icon">
                           <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="file-upload" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" className="svg-inline--fa fa-file-upload fa-w-12 fa-3x"><path fill="currentColor" d="M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zm65.18 216.01H224v80c0 8.84-7.16 16-16 16h-32c-8.84 0-16-7.16-16-16v-80H94.82c-14.28 0-21.41-17.29-11.27-27.36l96.42-95.7c6.65-6.61 17.39-6.61 24.04 0l96.42 95.7c10.15 10.07 3.03 27.36-11.25 27.36zM377 105L279.1 7c-4.5-4.5-10.6-7-17-7H256v128h128v-6.1c0-6.3-2.5-12.4-7-16.9z" className=""></path></svg>
                         </span>
+
                         <span className="file-label">
                           Select your sales file
                         </span>
                       </span>
+
                       <span className="file-name is-flex is-justify-content-center">
                         No file selected
                       </span>
