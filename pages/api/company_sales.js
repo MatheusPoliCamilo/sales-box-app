934e1848ddcb340dcfadee802e2523f5344060b9
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-08-17 03:03:56 -0300

    Add fs to the project

--- pages/api/company_sales.js
+++ pages/api/company_sales.js
@@ -1,4 +1,5 @@
 import formidable from "formidable";
+import { promises as fs } from 'fs'
 
 export const config = {
   api: {
@@ -10,12 +11,16 @@ export default async function createCompanySales (request, response) {
   const form = new formidable.IncomingForm()
 
   return form.parse(request, async function (_err, _fields, files) {
+    const file = await fs.readFile(files.file.path, {
+      encoding: 'utf8',
+    })
+
     const companySaleRequest = await fetch(`${process.env.API_URL}/company_sales`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ file_path: files.file.path }),
+      body: JSON.stringify({ file: file }),
     })
 
     const companySale = await companySaleRequest.json()
